1a4bb6209abb387ee8d9647a26cfd1741c95b979
wonaval <wonaval@gmail.com>
2021-12-13 01:35:18 -0500

    Fixed some route logic

--- seeders/seeders_electronics.js
+++ seeders/seeders_electronics.js
@@ -5,20 +5,20 @@ module.exports = {
   
     await queryInterface.bulkInsert('items', [
       { //product 1
-         name: 'Magic iPad',
-         description: 'Perfect for your studies at Hogwarts.',
-         price: 100000,
-         image: 'ipad.png',
-         quantity: 100,
-				 category: 'electronics',
-         createdAt: new Date(),
-         updatedAt: new Date()
+          name: 'Magic iPad',
+          description: 'Perfect for your studies at Hogwarts.',
+          price: 100000,
+          image: 'https://i.imgur.com/FjVvpYK.png',
+          quantity: 100,
+          category: 'electronics',
+          createdAt: new Date(),
+          updatedAt: new Date()
       },
       { //product 2
         name: 'Golden iPhone',
         description: 'The only phone for true ballers.',
         price: 3500,
-        image: 'iPhoneGold.png',
+        image: 'https://i.imgur.com/FjVvpYK.png',
         quantity: 5000,
         category: 'electronics',
         createdAt: new Date(),
@@ -26,47 +26,48 @@ module.exports = {
       },
       { //product 3
         name: 'iPokedex23',
-         description: 'Device designed to catalogue and provide information regarding various species of Pokémon. Latest version.',
-         price: 2500,
-         image: 'pokedex.png',
-         quantity: 10000,
-         category: 'electronics',
-         createdAt: new Date(),
-         updatedAt: new Date()
+          description: 'Device designed to catalogue and provide information regarding various species of Pokémon. Latest version.',
+          price: 2500,
+          image: 'https://i.imgur.com/FjVvpYK.png',
+          quantity: 10000,
+          category: 'electronics',
+          createdAt: new Date(),
+          updatedAt: new Date()
       },
       { //product 4
         name: 'Tezla flying car',
-         description: 'A car that flies. Beware of no fly zones.',
-         price: 900000,
-         image: 'teslaflyingcar.jpeg',
-         quantity: 100,
-         category: 'electronics',
-         createdAt: new Date(),
-         updatedAt: new Date()
+          description: 'A car that flies. Beware of no fly zones.',
+          price: 900000,
+          image: 'https://i.imgur.com/FjVvpYK.png',
+          quantity: 100,
+          category: 'electronics',
+          createdAt: new Date(),
+          updatedAt: new Date()
       },
       { //product 5
         name: 'Hologram projector',
-         description: 'Talk to users in real time with a life-sized hologram.',
-         price: 20000,
-         image: 'hologramprojector.jpeg',
-         quantity: 100,
-         category: 'electronics',
-         createdAt: new Date(),
-         updatedAt: new Date()
+          description: 'Talk to users in real time with a life-sized hologram.',
+          price: 20000,
+          image: 'https://i.imgur.com/FjVvpYK.png',
+          quantity: 100,
+          category: 'electronics',
+          createdAt: new Date(),
+          updatedAt: new Date()
       },
       { //product 6 
         name: 'Tesla Time Machine',
-         description: 'Travel back and fourth through specified periods of time.',
-         price: 1000000,
-         image: 'timeMachine.jpeg',
-         quantity: 10,
-         category: 'electronics',
-         createdAt: new Date(),
-         updatedAt: new Date()
+          description: 'Travel back and fourth through specified periods of time.',
+          price: 1000000,
+          image: 'https://i.imgur.com/FjVvpYK.png',
+          quantity: 10,
+          category: 'electronics',
+          createdAt: new Date(),
+          updatedAt: new Date()
       }
-     ])  
+      ])  
 
   },
+
 
   down: async (queryInterface, Sequelize) => {
     /**
