01e17a0272acdb7d93eb67ff5c4b0c8486f41220
wonaval <wonaval@gmail.com>
2021-12-13 23:58:32 -0500

    Fixed milisecond issue

--- models/item.js
+++ models/item.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.item.belongsToMany(models.user, {through: 'cart'})
     }
   };
   item.init({
--- models/user.js
+++ models/user.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.user.belongsToMany(models.item, {through: 'cart'})
     }
   };
   user.init({
